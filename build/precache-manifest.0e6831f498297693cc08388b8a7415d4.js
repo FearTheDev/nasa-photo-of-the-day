7a5b945f96731ee18a2060db019580e05cda6082
FearTheDev <w.m.t.b5115@gmail.com>
2019-09-13 03:11:52 -0400

    Update for Netlify

---
+++ build/precache-manifest.0e6831f498297693cc08388b8a7415d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1809e4f85b2e88a180ed87de00d83c22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a23298b968c2ee78f600",
+    "url": "/static/css/2.f2987ecf.chunk.css"
+  },
+  {
+    "revision": "b16223938637663b5384",
+    "url": "/static/css/main.77e79751.chunk.css"
+  },
+  {
+    "revision": "a23298b968c2ee78f600",
+    "url": "/static/js/2.61215275.chunk.js"
+  },
+  {
+    "revision": "b16223938637663b5384",
+    "url": "/static/js/main.a27e34ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
